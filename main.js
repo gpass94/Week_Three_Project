842196b17fcde40d4857f40cc7e212f5786ff210
Grant Paschal <gpass94@gmail.com>
2017-06-15 09:11:09 -0400

    Modified Commit

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
-const sevenBtn = document.querySelector("#seven");
-const eightBtn = document.querySelector("#eight");
+const clearBtn = document.querySelector(".clearBtn");
+const displayBtn = document.querySelector(".displayBtn")
+const sevenBtn = document.querySelector(".sevenBtn");
+const eightBtn = document.querySelector(".eightbtn");
+const nineBtn = document.querySelector(".ninebtn");
+const dividerBtn = document.querySelector(".dividerBtn");
+const sixBtn = document.querySelector(".sixBtn");
+const fiveBtn = document.querySelector(".fiveBtn");
+const fourBtn = document.querySelector(".fourBtn");
+const mulitplyBtn = document.querySelector(".mulitplyBtn");
+const threeBtn = document.querySelector(".threeBtn");
+const twoBtn = document.querySelector(".twoBtn");
+const oneBtn = document.querySelector(".oneBtn");
+const subtractBtn = document.querySelector(".subtractBtn");
+const zeroBtn = document.querySelector(".zeroBtn");
+const decimalBtn = document.querySelector(".decimalBtn");
+const equalBtn = document.querySelector(".equalBtn");
+const addBtn = document.querySelector(".addBtn");
 let btnList = document.querySelectorAll(".squares");
 
 
 for (let i = 0; i < btnList.length; i++) {
   let btn = btnList[i];
 
-  btn.addEventListener("click",function(){
-    console.log("You clicked button number " + i);
-  });
+  btn.addEventListener("onClick",function(){
+    if(btn.innerHTML == "=") {
+      let equation = displayBtn.innerHTML;
+      equation = equation.replace("x", "*");
+      displayBtn.innerHTML = eval(equation);
+
+      } else if(btn.innerHTML == "C") {
+     displayBtn.innerHTML = "";
+
+
+  } else {
+
+    displayBtn.innerHTML += btn.innerHTML;
+
+  }
+
+
+
+});
 }
